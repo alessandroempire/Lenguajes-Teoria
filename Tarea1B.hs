873928a96f867820c03e066fe1af616bd760dc6a
alessandroempire <alessandroempire@gmail.com>
2014-03-22 18:58:47 -0430

    Tarea 1 de la teoria, la pregunta B

--- Tarea1B.hs
+++ Tarea1B.hs
@@ -1,30 +1,37 @@
 --ejercicio B de la tarea 1 de lenguajes Teoria. 
 
-import Data.List
+import Data.Maybe
 
 data Proc = Proc (String, Integer, [Proc])
           deriving (Show, Eq)
 
 mem :: Proc -> Integer
-mem (Proc (_,i,[])) = i
---mem (Proc (a,i,ps)) = memAux (Proc (a,i,ps))
-  --  where memAux (Proc (x,y,ps)) = foldl (\counter -> ) 0 ps
+mem (Proc (name, weight, [])) = weight
+mem (Proc (name, weight, xs)) = weight + maximum (map mem xs)
 
-memAux (Proc (x,y, [])) contador = contador + y
-memAux (Proc (x,y,ps))  contador = foldl (funcion) 0 ps
-    where funcion (Proc (a,b,cs)) = 
+{-
+memRec :: Proc -> Maybe Integer
+memRec (Proc (name, weight, xs)) = memRec' (Proc (name, weight, xs)) []
+    where memRec' (Proc (name, weight, [])) listP = Just weight
+          memRec' (Proc (name, weight, xs)) listP = 
+              if elem name listP         then Nothing
+              else if elem Nothing value then Nothing
+                   else Just ( weight + fromJust (maximum value))
+                   where value = ( map (flip ( memRec' name:listP)) xs) FUCKING FLIP
+-}
 
---memAux1 (Proc (x,y,ps)) = map (memAux1) ps 
+memRec :: Proc -> Maybe Integer
+memRec (Proc (name, weight, xs)) = memRec' [] (Proc (name, weight, xs)) 
 
-proc1 = Proc ("caso base", 6, [])
+memRec' :: [String] -> Proc -> Maybe Integer
+memRec' listP (Proc (name, weight, []))  = Just weight
+memRec' listP (Proc (name, weight, xs))  = if elem name listP         then Nothing
+                                           else if elem Nothing value then Nothing
+                                                else Just ( weight + fromJust (maximum value))
+                                                where value = (map (memRec' (name:listP)) xs)
 
-proc2 = Proc ("caso 2", 3, [proc1])
 
-proc3 = Proc ("caso3", 5, [proc2, proc1])
-
-
-procA = Proc ("a", 6, [procB, procC])
-procB = Proc ("b", 5, [procC, procD])
-procC = Proc ("c", 4, [procE])
-procD = Proc ("d", 2, [])
-procE = Proc ("e", 1, [])
+--Ejemplo de prueba
+procA = Proc ("a", 10, [procB, procC])
+procB = Proc ("b", 15, [procC])
+procC = Proc ("c", 10, [procA])
