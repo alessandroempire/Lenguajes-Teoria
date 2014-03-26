fc5cacafa11a66b6947a9a328b0252ee2b0e3a22
alessandroempire <alessandroempire@gmail.com>
2014-03-25 21:23:20 -0430

    Tarea1G, implementar la parte de completoFR y funciona al pelo

--- Tarea1G.hs
+++ Tarea1G.hs
@@ -9,23 +9,29 @@ takeName (Proc (name,_,_)) = name
 completo :: [Proc] -> Bool
 completo [] = True
 completo ps = completoAux ps []
-    where completoAux :: [Proc] -> [String] -> Bool
-          completoAux [] xs                      = True 
---          completoAux ((Proc (name,_,[])):ps) xs = completoAux ps (name:xs)
+    where completoAux [] _                       = True 
           completoAux ((Proc (name,_,ls)):ps) xs = 
-              if and (map (flip (elem) xs) (map takeName ls)) then completoAux ps (name:xs)
+              if and (map (flip (elem) xs) (map takeName ls)) 
+              then completoAux ps (name:xs)
               else False
 
 completoFR :: [Proc] -> Bool
-completoFR = undefined
+completoFR [] = True
+completoFR ps = completoFRaux ps (map takeName ps)
+    where completoFRaux [] _                       = True
+          completoFRaux ((Proc (name,_,ls)):ps) xs = 
+              if and (map (flip (elem) xs) (map takeName ls))
+              then completoFRaux ps xs
+              else False
 
 --Ejemplos para probar
 a = Proc ("a", 10, [b])
 b = Proc ("b", 10, [c])
-c = Proc ("c", 10, [])
+c = Proc ("c", 10, [e])
+e = Proc ("e", 10, [])
 
 m = Proc ("main", 20, [a])
 
 list1 = [c]
 list  = [c,b,a,m]
-listF = [m,c,b,a]
+listF = [m,c,b,a,e]
